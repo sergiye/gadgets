330db8de95c5a8a449dc908163a879e1cb21b592
Sergiy Egoshyn <egoshin.sergey@gmail.com>
2025-07-26 15:02:36 +0300

    black clock patch for system clock gadget

---
+++ blackClock/register.cmd
@@ -0,0 +1,2 @@
+xcopy monitor*.png "c:\Program Files\Windows Sidebar\Gadgets\Clock.Gadget\images" /q /y
+xcopy monitor*.png "c:\Program Files (x86)\Windows Sidebar\Gadgets\Clock.Gadget\images" /q /y
