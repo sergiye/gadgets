8ed5c9ddd5256442b6224454db97b9e335b530d8
Sergiy Egoshyn <egoshin.sergey@gmail.com>
2025-07-27 16:52:43 +0300

    patch for system calendar gadget to make it blue

---
+++ blackCalendar/register.cmd
@@ -0,0 +1,5 @@
+xcopy css\*.css "c:\Program Files\Windows Sidebar\Gadgets\Calendar.Gadget\css" /q /y
+xcopy images\*.png "c:\Program Files\Windows Sidebar\Gadgets\Calendar.Gadget\images" /q /y
+
+rem xcopy css\*.css "c:\Program Files (x86)\Windows Sidebar\Gadgets\Calendar.Gadget\css" /q /y
+rem xcopy images\*.png "c:\Program Files (x86)\Windows Sidebar\Gadgets\Calendar.Gadget\images" /q /y
