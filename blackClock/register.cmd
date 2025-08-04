da376a4ab0ce1096119d0e2b7a33387b2f3ba288
Sergiy Egoshyn <egoshin.sergey@gmail.com>
2025-08-04 22:47:00 +0300

    Sergiye Clock 2.0

--- blackClock/register.cmd
+++
@@ -1,2 +0,0 @@
-xcopy monitor*.png "c:\Program Files\Windows Sidebar\Gadgets\Clock.Gadget\images" /q /y
-xcopy monitor*.png "c:\Program Files (x86)\Windows Sidebar\Gadgets\Clock.Gadget\images" /q /y
